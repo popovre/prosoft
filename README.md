3cedf999431897fa4a0a3a2a1e018aa38aa1b773
Roman <popov.roman.e@gmail.com>
2024-07-05 01:14:02 +0500

    get started

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+## Expanding the ESLint configuration
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.root}>
+      <div className={styles.content}>Vite + React</div>
+    </div>
+  );
+}
+
+export default App;
