9c206a98922a5379e986094a02e21ae11f826f9c
popovre <popov.roman.e@gmail.com>
2024-07-05 14:43:29 +0500

    make theme mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
+import clsx from 'clsx';
 
 function App() {
+  const themePreferences = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+
+  const [isDark, setIsDark] = useState(themePreferences);
+
+  useEffect(() => {
+    const localThemeMode = localStorage.getItem('isDark');
+    const darkMode =
+      localThemeMode === null ? themePreferences : JSON.parse(localThemeMode);
+
+    setIsDark(darkMode);
+  }, [isDark, themePreferences]);
+
   return (
-    <div className={styles.root}>
+    <div className={styles.root} data-theme={isDark ? 'dark' : 'light'}>
       <div className={styles.content}>Vite + React</div>
+      <button
+        className={clsx(styles.themeToggle)}
+        onClick={() => {
+          localStorage.setItem('isDark', JSON.stringify(!isDark));
+          setIsDark(!isDark);
+        }}
+      >
+        Theme toggle
+      </button>
     </div>
   );
 }
